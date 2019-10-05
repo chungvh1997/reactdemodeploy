85e25a78bf07b7b5837dd40e056dc6f6859d0838
chungvh <chungvh@runsystem.net>
2019-10-06 00:53:37 +0700

    A

---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class AboutUs extends Component {
+  state = {};
+
+  render() {
+    return <div>About Us</div>;
+  }
+}
+
+export default AboutUs;
---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {};
+
+  render() {
+    return <div>chung co ho</div>;
+  }
+}
+
+export default Home;
