041836074b0de2067a306fbcf24d1d65daa4f3c1
chungvh <chungvh@runsystem.net>
2019-10-06 00:58:26 +0700

    Updates

---
+++ precache-manifest.8ca0f35772d9ca1dfd5c9913e01f42cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5f2044133f4927fe3d9",
+    "url": "/reactdemodeploy/static/js/runtime~main.f71bb08b.js"
+  },
+  {
+    "revision": "cb59c84b78cbe68663d9",
+    "url": "/reactdemodeploy/static/js/main.a0f9938a.chunk.js"
+  },
+  {
+    "revision": "e66b33066c742fe5562a",
+    "url": "/reactdemodeploy/static/js/2.65a1fa70.chunk.js"
+  },
+  {
+    "revision": "cb59c84b78cbe68663d9",
+    "url": "/reactdemodeploy/static/css/main.af1a3219.chunk.css"
+  },
+  {
+    "revision": "e66b33066c742fe5562a",
+    "url": "/reactdemodeploy/static/css/2.ad25e959.chunk.css"
+  },
+  {
+    "revision": "438a1b22d3a409cbaab07427b9b91f1f",
+    "url": "/reactdemodeploy/index.html"
+  }
+];
