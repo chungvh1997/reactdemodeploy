dfc654e4ab837149b9a4bcb12b8b1f723133322a
chungvh <chungvh@runsystem.net>
2019-10-06 15:28:04 +0700

    Updates

---
+++ precache-manifest.eb940c88b7363bd742cf475428edf95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48960c44377b81ed226f",
+    "url": "/reactdemodeploy/npm/static/js/runtime~main.880258f9.js"
+  },
+  {
+    "revision": "71051bc156e7c0718d25",
+    "url": "/reactdemodeploy/npm/static/js/main.6d964390.chunk.js"
+  },
+  {
+    "revision": "82e0de93bed30fbcf75d",
+    "url": "/reactdemodeploy/npm/static/js/2.82142d7d.chunk.js"
+  },
+  {
+    "revision": "71051bc156e7c0718d25",
+    "url": "/reactdemodeploy/npm/static/css/main.7bca10ab.chunk.css"
+  },
+  {
+    "revision": "82e0de93bed30fbcf75d",
+    "url": "/reactdemodeploy/npm/static/css/2.ad25e959.chunk.css"
+  },
+  {
+    "revision": "e1af300cb0a520f6929c6a46e6712439",
+    "url": "/reactdemodeploy/npm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactdemodeploy/precache-manifest.e7794846a770e6f0d50cb0d2b579cb91.js"
+  "/reactdemodeploy/npm/precache-manifest.eb940c88b7363bd742cf475428edf95c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactdemodeploy/index.html", {
+workbox.routing.registerNavigationRoute("/reactdemodeploy/npm/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
